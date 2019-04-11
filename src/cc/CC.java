d318455464683021152a96471ef7d69f0ee4bded
darckestra <darckestra@gmail.com>
2019-04-11 12:05:13 -0500

    subida2

--- src/cc/CC.java
+++ src/cc/CC.java
@@ -15,7 +15,8 @@ public class CC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login l = new Login();
+        l.setVisible(true);
     }
     
 }
