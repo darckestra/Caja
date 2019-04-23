b46610351ef6c8c48673464ba7905889c2009c56
darckestra <darckestra@gmail.com>
2019-04-23 14:54:38 -0500

    actualizarSQL

--- src/cc/Buscar.java
+++ src/cc/Buscar.java
@@ -57,6 +57,8 @@ private TableRowSorter trsfiltro;
         txtBuscar = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         tblBuscar = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        txtdos = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setUndecorated(true);
@@ -91,7 +93,19 @@ private TableRowSorter trsfiltro;
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tblBuscar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblBuscarMouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(tblBuscar);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -106,6 +120,12 @@ private TableRowSorter trsfiltro;
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(txtBuscar)))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(txtdos, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(78, 78, 78)
+                .addComponent(jButton1)
+                .addGap(74, 74, 74))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -116,7 +136,10 @@ private TableRowSorter trsfiltro;
                     .addComponent(txtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 446, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(txtdos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
         pack();
@@ -161,6 +184,52 @@ private TableRowSorter trsfiltro;
     private void txtBuscarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBuscarKeyReleased
      //   filtro(txtBuscar.getText().toUpperCase(), tblBuscar);
     }//GEN-LAST:event_txtBuscarKeyReleased
+
+    private void tblBuscarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblBuscarMouseClicked
+       int col = tblBuscar.getSelectedRow();        
+        //txtProveedor.setText(tblProveedor.getModel().getValueAt(col, 0).toString());
+        txtBuscar.setText(tblBuscar.getValueAt(tblBuscar.getSelectedRow(), 3).toString());
+        txtdos.setText(tblBuscar.getValueAt(tblBuscar.getSelectedRow(), 0).toString());
+        
+    }//GEN-LAST:event_tblBuscarMouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String en,donde="",total,cadena="";
+        int pp,ppp;
+        char c = 0;
+        en=txtBuscar.getText();        
+        //for (int n = 0, n <str.length (); n + +) { char c = str.charAt (n); System.out.println (c); }
+        
+            for (int i = 0; i < en.length(); i++) {
+                c = en.charAt(i);                
+                    if (c == '.') {
+                    break;
+                    }else{
+                        cadena=cadena+c;
+                }
+            }        
+        pp= Integer.parseInt(cadena);
+        ppp=pp-1;
+        total=Integer.toString(ppp);        
+        donde=txtdos.getText();
+        String sql="update productos set existencia = ? where codigo = ?";
+        
+        try {
+           sent = conn.createStatement();
+           //ResultSet rs = sent.executeQuery();
+           PreparedStatement pstmt =conn.prepareStatement(sql);
+           pstmt.setString(1, total);
+           pstmt.setString(2, donde);
+           pstmt.executeUpdate();
+//           ResultSet rs = pstmt.executeQuery();
+//            JOptionPane.showMessageDialog(null, pstmt);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        llenar();
+        txtBuscar.setText("");
+        txtdos.setText("");
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     public void llenar(){
        DefaultTableModel model = new DefaultTableModel();
@@ -225,10 +294,12 @@ private TableRowSorter trsfiltro;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tblBuscar;
     private javax.swing.JTextField txtBuscar;
+    private javax.swing.JTextField txtdos;
     // End of variables declaration//GEN-END:variables
 
 }
